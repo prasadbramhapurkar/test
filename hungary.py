7b89c98af2a3324b2b25b360b5bf39f1041de54a
Prasad <prasadsb2424@gmail.com>
2023-01-21 13:00:47 +0530

    fries

--- hungary.py
+++ hungary.py
@@ -1,5 +1,8 @@
 hungary = input("are you hungary?")
 if(hungary=="yes"):
     print("eat samosa")
+    print("eat pizza")
+    print("eat burger")
+    print("eat fries")
 else:
     print("do not eat")
